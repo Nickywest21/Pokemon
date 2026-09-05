74c1ced4ed897b742a914300e514849f9d25a940
Juan Pablo <juanpaestra8@gmail.com>
2026-09-05 14:13:17 -0600

    Agregue el segundo endpoint para buscar por "TIPO"

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { getPokemon } from "./controllers/pokemon";
+import { getPokemonTipo } from "./controllers/tipo";
 
 export const router = Router();
 
 // método + dirección → controlador. La ruta NO resuelve nada.
 router.get("/pokemon/:nombre", getPokemon);
+router.get("/tipo/:tipo", getPokemonTipo);
