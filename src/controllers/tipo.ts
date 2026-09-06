5964b0be465e174f31c012bfb06ffc5b97c4370f
OJortiz <OJORTIZP@correo.url.edu.gt>
2026-09-05 22:39:24 -0600

    Squashed commit of the following:
    commit 74c1ced4ed897b742a914300e514849f9d25a940
    Author: Juan Pablo <juanpaestra8@gmail.com>
    Date:   Sat Sep 5 14:13:17 2026 -0600
    
        Agregue el segundo endpoint para buscar por "TIPO"
    
    commit 04edec9ab290b187188f4e4d4164fac3d45c3791
    Author: Juan Pablo <juanpaestra8@gmail.com>
    Date:   Sat Sep 5 14:03:31 2026 -0600
    
        cambios en la estructura y funciona correctamente
    
    commit eb1aff4f0ef6d3f3619f984eeaa3bb1a9aa8b7cc
    Author: Juan Pablo <juanpaestra8@gmail.com>
    Date:   Thu Sep 3 21:03:19 2026 -0600
    
        Estructura arreglada

---
+++ src/controllers/tipo.ts
@@ -0,0 +1,20 @@
+import type { Request, Response } from "express";
+
+export async function getPokemonTipo(req: Request, res: Response) {
+    const { tipo } = req.params;
+
+    const r = await fetch(`https://pokeapi.co/api/v2/type/${tipo}`);
+
+    if (!r.ok) {
+        return res.status(404).json({
+            error: "No encontré ese tipo"
+        });
+    }
+
+    const data = await r.json();
+
+    res.json({
+        tipo: data.name,
+        pokemon: data.pokemon.map((p: any) => p.pokemon.name)
+    });
+}
